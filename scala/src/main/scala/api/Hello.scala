b558e93158fff8e55f26cb30246703957951db15
Alex Burley <alexburley3@gmail.com>
2020-06-17 22:33:50 +0100

    More init

---
+++ scala/src/main/scala/api/Hello.scala
@@ -0,0 +1,9 @@
+package api
+
+object Hello extends Greeting with App {
+  println(greeting)
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
--- scala/src/test/scala/example/HelloSpec.scala
+++ scala/src/test/scala/example/HelloSpec.scala
@@ -1,4 +1,4 @@
-package example
+package api
 
 import org.scalatest._
 
