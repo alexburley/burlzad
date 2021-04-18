0c824a4c31e11f6f96bb4818e8a7f5f5f3bb2c2f
Alex <alexburley3@live.co.uk>
2021-04-18 22:47:39 +0100

    Add books

--- src/App/Content/BooksContent/items.js
+++ src/App/Content/BooksContent/items.js
@@ -33,13 +33,61 @@ export default [
     imgSrc:
       "https://images-na.ssl-images-amazon.com/images/I/41JOmGowq-L._SX258_BO1,204,203,200_.jpg",
   },
-
   {
     title: "Head First Design Patterns",
     yearRead: 2018,
     author: "Eric Freeman",
     imgSrc:
       "https://images-eu.ssl-images-amazon.com/images/I/61APhXCksuL._SX218_BO1,204,203,200_QL40_ML2_.jpg",
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    yearRead: 2018,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/51UoqRAxwEL.jpg",
+  },
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    yearRead: 2018,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/51TA3VfN8RL._SY346_.jpg",
+  },
+  {
+    title: "Harry Potter and the Prisoner of Azkaban",
+    yearRead: 2018,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/51Dfqo6jR5L.jpg",
+  },
+  {
+    title: "Harry Potter and the Goblet of the Fire",
+    yearRead: 2018,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/51Vjb2qJwzL.jpg",
+  },
+  {
+    title: "Harry Potter and the Order of the Phoenix",
+    yearRead: 2018,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/51-SI2+aQ2L.jpg",
+  },
+  {
+    title: "Harry Potter and the Half-Blood Prince",
+    yearRead: 2019,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/51myHyjJsyL._SY346_.jpg",
+  },
+  {
+    title: "Harry Potter and the Deathly Hallows",
+    yearRead: 2019,
+    author: "J.K. Rowling",
+    imgSrc: "https://m.media-amazon.com/images/I/511DhzIj4cL._SY346_.jpg",
+  },
+  {
+    title: "Histories of Nations",
+    yearRead: 2019,
+    author: "Peter Furtado",
+    imgSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/518c55YRyYL._SX326_BO1,204,203,200_.jpg",
   },
   {
     title: "The Lord of the Rings",
@@ -75,6 +123,20 @@ export default [
     author: "JRR Tolkien",
     imgSrc:
       "https://cdn.waterstones.com/bookjackets/large/9780/2611/9780261103283.jpg",
+  },
+  {
+    title: "A Wizard of Sea",
+    yearRead: 2019,
+    author: "Ursula K. Le Guin",
+    imgSrc:
+      "https://images-eu.ssl-images-amazon.com/images/I/51iQfxB5rHL._SY291_BO1,204,203,200_QL40_ML2_.jpg",
+  },
+  {
+    title: "Tombs of Atuan",
+    yearRead: 2020,
+    author: "Ursula K. Le Guin",
+    imgSrc:
+      "https://images-eu.ssl-images-amazon.com/images/I/51tLvYdbRcL._SY291_BO1,204,203,200_QL40_ML2_.jpg",
   },
   {
     title: "The Path of Daggers",
@@ -131,5 +193,26 @@ export default [
     author: "Thomas Hunter",
     imgSrc:
       "https://m.media-amazon.com/images/I/81S87xwKCvL._AC_UY436_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "The Design of Everyday Things",
+    yearRead: 2021,
+    author: "Donald Norman",
+    imgSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/41bWcNdTGmL._SX330_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "Gardens of the Moon",
+    yearRead: 2021,
+    author: "Steven Erikson",
+    imgSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/41iY-pZ9PAL._SX298_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "The Farthest Shore",
+    yearRead: 2021,
+    author: "Ursula K. Le Guin",
+    imgSrc:
+      "https://images-eu.ssl-images-amazon.com/images/I/41TUQacykuL._SY291_BO1,204,203,200_QL40_ML2_.jpg",
   },
 ];
