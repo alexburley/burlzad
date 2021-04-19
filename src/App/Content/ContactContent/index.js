959d21fdedd9f4ec49697077f77b51fa25c51cb8
Alex <alexburley3@live.co.uk>
2021-04-19 21:44:32 +0100

    Contact

--- src/App/Content/ContactContent/index.js
+++ src/App/Content/ContactContent/index.js
@@ -3,9 +3,14 @@ import "./ContactContent.css";
 
 export default function ContactContent() {
   return (
-    <p>
-      If you want to get in touch, you can contact me on{" "}
-      <a href="https://www.linkedin.com/in/alex-burley-083001103/">LinkedIn</a>.
-    </p>
+    <div className="ContactContent">
+      <p>
+        If you want to get in touch, you can contact me on{" "}
+        <a href="https://www.linkedin.com/in/alex-burley-083001103/">
+          LinkedIn
+        </a>
+        .
+      </p>
+    </div>
   );
 }
