6842e18eab18eac3881f6a2354140599a821a08a
Alex <alexburley3@live.co.uk>
2021-04-22 18:52:26 +0100

    Add better contact page

--- src/App/Content/ContactContent/index.js
+++ src/App/Content/ContactContent/index.js
@@ -4,13 +4,22 @@ import "./ContactContent.css";
 export default function ContactContent() {
   return (
     <div className="ContactContent">
-      <p>
-        If you want to get in touch, you can contact me on{" "}
-        <a href="https://www.linkedin.com/in/alex-burley-083001103/">
-          LinkedIn
-        </a>
-        .
-      </p>
+      <a href="https://www.linkedin.com/in/alex-burley/">
+        <div className="ImageWrapper">
+          <img
+            src="http://assets.stickpng.com/images/58e91afdeb97430e81906504.png"
+            alt="LinkedIn"
+          />
+        </div>
+      </a>
+      <a href="https://github.com/alexburley">
+        <div className="ImageWrapper">
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="LinkedIn"
+          />
+        </div>
+      </a>
     </div>
   );
 }
