75f9dfae9358250507be310273eae41f9250fb2b
Alex <alexburley3@live.co.uk>
2021-04-24 17:08:26 +0100

    Delete content

--- src/App/Content/index.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import { Home } from "../Pages";
-import { Container } from "@material-ui/core";
-
-export default function Content() {
-  const [content, setContent] = useState(Home);
-  return <Container maxWidth="md">{content}</Container>;
-}
