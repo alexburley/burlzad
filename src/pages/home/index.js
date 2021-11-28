61feaf9267b80026b37ff2973cafcf449fe43d54
Alex Burley <alexburley3@live.co.uk>
2021-11-28 19:01:49 +0000

    Update profile page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,49 +1,57 @@
-import { Link, makeStyles, Typography } from "@material-ui/core";
-import { Code, GitHub, LinkedIn } from "@material-ui/icons";
+import { Code, GitHub, Linkedin } from "react-feather";
 import React from "react";
-
-const useStyles = makeStyles({
-  "banner-block": {
-    textAlign: "center",
-    paddingTop: "100px",
-  },
-  "banner-h1": {
-    fontWeight: "bold",
-  },
-  icons: {
-    paddingTop: "10px",
-  },
-});
+import styled from "styled-components";
 
 export default function HomePage() {
-  const classes = useStyles();
   return (
-    <div className={`${classes["banner-block"]}`}>
-      <Typography variant="h1" className={`${classes["banner-h1"]}`}>
-        Alex Burley
-      </Typography>
-      <Typography variant="h4" className={`${classes["banner-h4"]}`}>
-        Backend Software Engineer
-      </Typography>
-      <div className={classes.icons}>
-        <span>
-          {[
-            { Icon: LinkedIn, url: "https://www.linkedin.com/in/alex-burley" },
-            { Icon: GitHub, url: "https://github.com/alexburley" },
-            { Icon: Code, url: "https://www.npmjs.com/~burlzad" },
-          ].map(({ Icon, url }, index) => (
-            <Link href={url} color="inherit" key={index}>
-              <Icon
-                style={{
-                  width: 75,
-                  height: 75,
-                  padding: "10px",
-                }}
-              />
-            </Link>
-          ))}
-        </span>
-      </div>
-    </div>
+    <BannerWrapper>
+      <NameTitle>Alex Burley</NameTitle>
+      <PositionTitle>Senior Software Engineer</PositionTitle>
+      <IconsWrapper>
+        {[
+          { Icon: GitHub, url: "https://github.com/alexburley" },
+          { Icon: Linkedin, url: "https://www.linkedin.com/in/alex-burley" },
+          { Icon: Code, url: "https://www.npmjs.com/~burlzad" },
+        ].map(({ Icon, url }, index) => (
+          <Link href={url} color="inherit" key={index}>
+            <Icon
+              style={{
+                width: 75,
+                height: 75,
+                padding: "10px",
+              }}
+            />
+          </Link>
+        ))}
+      </IconsWrapper>
+    </BannerWrapper>
   );
 }
+
+const BannerWrapper = styled.div`
+  text-align: center;
+  margin-top: 100px;
+`;
+const NameTitle = styled.h1`
+  font-weight: bold;
+  font-size: 6rem;
+  line-height: 120%;
+`;
+const PositionTitle = styled.h2`
+  font-size: 2rem;
+  font-weight: 600;
+`;
+const IconsWrapper = styled.div`
+  padding-top: 10px;
+  display: flex;
+  gap: 18px;
+  justify-content: center;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: inherit;
+  :hover {
+    color: var(--color-secondary);
+  }
+`;
