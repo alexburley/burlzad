c843da14298bc7d0f5475a6d4dca6ed471d2fdb0
alexburley <52782469+alexburley@users.noreply.github.com>
2026-02-22 20:44:57 +0000

    chore: Update books

--- src/pages/books/items.js
+++ src/pages/books/items.js
@@ -1,5 +1,29 @@
 export default [
   {
+    title: "Assassin's Fate",
+    yearRead: 2026,
+    author: "Robin Hobb",
+    fiction: true,
+    imgSrc:
+      "https://m.media-amazon.com/images/I/91mdDiJRquL._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    title: "Fool's Quest",
+    yearRead: 2026,
+    author: "Robin Hobb",
+    fiction: true,
+    imgSrc:
+      "https://m.media-amazon.com/images/I/91jmp2PbsUL._UF1000,1000_QL80_.jpg",
+  },
+  {
+    title: "On The Shortness of Life (De Brevitate Vitae)",
+    yearRead: 2026,
+    author: "Seneca",
+    fiction: false,
+    imgSrc:
+      "https://m.media-amazon.com/images/I/51lHtfG673L._AC_UF894,1000_QL80_.jpg",
+  },
+  {
     title: "Fool's Quest",
     yearRead: 2026,
     author: "Robin Hobb",
